2857b5232ffb5a9367d06ec200b6c29238dd9f05
crenega2 <caydenextra1@gmail.com>
2023-08-30 16:59:55 -0400

    Update exercise_1.py

--- exercise_1.py
+++ exercise_1.py
@@ -1,4 +1,4 @@
-# Write a script that takes in a grade from 0-100 inclusive (include both 0 and 100 in the range). Assuming a normal 10 point grading scale, print off whether the user got an A, B, C, D, or F.
+# write a script that takes in a grade from 0-100 inclusive (include both 0 and 100 in the range). Assuming a normal 10 point grading scale, print off whether the user got an A, B, C, D, or F.
 
 number = int(input("Please enter your grade: "))
 print(number)
