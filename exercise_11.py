ca78c08f0e720e060a208779236624ffbb03e817
crenega2 <143453364+crenega2@users.noreply.github.com>
2023-08-31 13:54:42 -0400

    Add files via upload

---
+++ exercise_11.py
@@ -0,0 +1,8 @@
+# Write a script that takes in a string from the user. Print the string out backwards.
+
+#[::-1] will print a string backwards 
+input = (input("Please enter a word: "))[::-1]
+print(input)
+
+
+
